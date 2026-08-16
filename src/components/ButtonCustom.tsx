5f321d0f5bd7367223b9afed26ee25c649f285c2
alvv <alvv2115@gmail.com>
2026-08-16 18:23:27 +0200

    feat: created hero section

---
+++ src/components/ButtonCustom.tsx
@@ -0,0 +1,34 @@
+import { Button, ButtonProps as HeadlessButtonProps } from "@headlessui/react";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+interface ButtonCustomProps extends HeadlessButtonProps {
+  children?: ReactNode;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+}
+
+export default function ButtonCustom({
+  children,
+  leftIcon,
+  rightIcon,
+
+  className,
+  type = "button",
+  ...props
+}: ButtonCustomProps) {
+  return (
+    <Button
+      type={type}
+      className={cn(
+        "inline-flex items-center justify-center gap-2 px-4 py-2 rounded transition-all cursor-pointer bg-primary hover:bg-primary/80  font-bold ",
+
+        typeof className === "string" ? className : "",
+      )}
+      {...props}
+    >
+      {leftIcon}
+      {children && <span>{children}</span>}
+      {rightIcon}
+    </Button>
+  );
+}
