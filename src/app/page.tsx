fed4fca257934ca82287d59155f620998ba94b4e
alvv <alvv2115@gmail.com>
2026-08-16 18:39:36 +0200

    feat: added features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Features from "@/components/Features/Features";
 import Hero from "@/components/Hero/Hero";
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Features />
     </main>
   );
 }
